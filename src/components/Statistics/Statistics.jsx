7930c8a719c9ede56156b51df6b07d4ef16b0149
eshansaif <35582062+eshansaif@users.noreply.github.com>
2023-04-14 02:05:18 +0600

    statics portion - area chart has been implemented in the stat section

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+    { "assignment": 1, "marks": 60 },
+    { "assignment": 2, "marks": 58 },
+    { "assignment": 3, "marks": 60 },
+    { "assignment": 4, "marks": 50 },
+    { "assignment": 5, "marks": 60 },
+    { "assignment": 6, "marks": 60 },
+    { "assignment": 7, "marks": 60 },
+    { "assignment": 8, "marks": 60 }
+];
 
 const Statistics = () => {
     return (
-        <div>
-            <h1>This is statistics page</h1>
-        </div>
+        <ResponsiveContainer width="100%" height={300}>
+            <AreaChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="assignment" />
+                <YAxis />
+                <Tooltip />
+                <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+        </ResponsiveContainer>
     );
 };
 
